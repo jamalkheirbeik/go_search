4336bbcf433a1c8aaf1bc594290170aa6fd85640
jamalkheirbeik <jamalkheirbeik@gmail.com>
2024-09-06 16:16:56 +0300

    refactored lexer.go

--- lexer.go
+++ lexer.go
@@ -11,40 +11,40 @@ type lexer struct {
 }
 
 // trims whitespaces and non printable characters
-func left_trim(lexer *lexer) {
-	for len(lexer.content) > 0 && (lexer.content[0] > unicode.MaxASCII || unicode.IsSpace(rune(lexer.content[0]))) {
-		lexer.content = lexer.content[1:]
+func (l *lexer) left_trim() {
+	for len(l.content) > 0 && (l.content[0] > unicode.MaxASCII || unicode.IsSpace(rune(l.content[0]))) {
+		l.content = l.content[1:]
 	}
 }
 
-func chop(lexer *lexer, n int) string {
-	token := lexer.content[0:n]
-	lexer.content = lexer.content[n:]
+func (l *lexer) chop(n int) string {
+	token := l.content[0:n]
+	l.content = l.content[n:]
 	return token
 }
 
-func chop_while(lexer *lexer, predicate func(rune) bool) string {
+func (l *lexer) chop_while(predicate func(rune) bool) string {
 	n := 0
-	for n < len(lexer.content) && predicate(rune(lexer.content[n])) && lexer.content[n] <= unicode.MaxASCII {
+	for n < len(l.content) && predicate(rune(l.content[n])) && l.content[n] <= unicode.MaxASCII {
 		n += 1
 	}
-	return chop(lexer, n)
+	return l.chop(n)
 }
 
-func next_token(lexer *lexer) (string, error) {
-	left_trim(lexer)
+func (l *lexer) next_token() (string, error) {
+	l.left_trim()
 
-	if len(lexer.content) == 0 {
+	if len(l.content) == 0 {
 		return "", errors.New("empty token")
 	}
 
-	if unicode.IsNumber(rune(lexer.content[0])) {
-		return chop_while(lexer, unicode.IsNumber), nil
+	if unicode.IsNumber(rune(l.content[0])) {
+		return l.chop_while(unicode.IsNumber), nil
 	}
 
-	if unicode.IsLetter(rune(lexer.content[0])) {
-		return strings.ToLower(chop_while(lexer, unicode.IsLetter)), nil
+	if unicode.IsLetter(rune(l.content[0])) {
+		return strings.ToLower(l.chop_while(unicode.IsLetter)), nil
 	}
 
-	return chop(lexer, 1), nil
+	return l.chop(1), nil
 }
