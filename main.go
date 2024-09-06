62f78e11e897c8d048d7b01a271b0bff295fbc9e
jamalkheirbeik <jamalkheirbeik@gmail.com>
2024-09-06 16:21:02 +0300

    deprecated json model and introduced concurrent file insertion to db

--- main.go
+++ main.go
@@ -1,51 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
-
-func list_commands() {
-	fmt.Println("Usage <program> <subcommand> [args]")
-	fmt.Println("    index <directory>           recursively indexes all files in directory and generates an index.json file containing frequency table")
-	fmt.Println("    search <query> <page>       Searches within index.json, ranks the result using TF-IDF, divides the result into pages and returns the data related to the provided page")
-	fmt.Println("    serve [port]                Serves a local HTTP server on port 8080 by default")
-}
-
 func main() {
-	if len(os.Args) > 1 {
-		switch os.Args[1] {
-		case "help":
-			list_commands()
-		case "index":
-			if len(os.Args) > 2 {
-				generate_index_file(os.Args[2])
-			} else {
-				fmt.Println("ERROR: No directory has been provided")
-			}
-		case "search":
-			if len(os.Args) > 3 {
-				query := os.Args[2]
-				if len(query) == 0 {
-					fmt.Println("ERROR: search query cannot be empty")
-				} else {
-					page, _ := strconv.Atoi(os.Args[3])
-					search(query, page)
-				}
-			} else {
-				fmt.Println("ERROR: Missing arguements. Use the help command to check out the usage.")
-			}
-		case "serve":
-			port := "8080"
-			if len(os.Args) > 2 {
-				port = os.Args[2]
-			}
-			serve(port)
-		default:
-			fmt.Println("ERROR: Unknown subcommand, to list available commands run 'go run main.go help'")
-		}
-	} else {
-		fmt.Println("ERROR: No subcommand has been provided, to list available commands run 'go run main.go help'")
-	}
+	db := NewDB()
+	db.generate_tables()
+	defer db.disconnect()
+	go db.add_dir_files("documents")
+	s := NewServer(db)
+	s.serve(":8080")
 }
